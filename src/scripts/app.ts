c90a007701e296330ddd3e07c09c4131f8ef0ae2
anu <flameforge@gmailc.om>
2022-02-15 21:47:17 +0100

    header section

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,7 +2,7 @@ import '../styles/app.scss';
 
 import { Greeter } from './greeter';
 
-const greeter: Greeter = new Greeter('wagawin');
+const greeter: Greeter = new Greeter('Dynamic Jigsaw puzzle', 'Bruno Palma');
 const app = document.getElementById('app');
 if (app) {
   greeter.start(app);
--- src/scripts/greeter.ts
+++ src/scripts/greeter.ts
@@ -1,12 +1,18 @@
 export class Greeter {
-  public constructor(public greeting: string) {}
+  public constructor(public greeting: string, public author: string) { }
   public start(container: HTMLElement | null): void {
     if (!container) {
       throw new Error('no container');
     }
+    const header = document.createElement('header');
     const h1 = document.createElement('h1');
     h1.textContent = `Welcome to ${this.greeting}!`;
-    container.classList.add('greeter');
-    container.appendChild(h1);
+
+    const subtitle = document.createElement('p');
+    subtitle.innerHTML = `Developed with love by <em><a href="https://about.me/bpalma/getstarted" target="_blank">${this.author}</a></em>`;
+
+    header.appendChild(h1);
+    header.appendChild(subtitle);
+    container.appendChild(header);
   }
 }
